639052e798370052fbafcac1020ad04c0edaf7b1
1zuu <isurualagiyawanna9717@gmail.com>
2021-07-07 18:14:46 +0530

    Remove Files

--- Inventory_Management_System/Inventory_Management_System/urls.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('Inference/', include("Inference.urls"), name="predict")
-]
